538a62255e6f5895c1e67d779392dd9258af37cb
Lucas Duarte <30901918+lusoal@users.noreply.github.com>
2023-08-03 11:58:41 -0500

    Update gptj-training.py

--- gptj-training.py
+++ gptj-training.py
@@ -44,8 +44,8 @@ storage_path="s3://testing-fine-tuning-jakhs"
 
 # TBD: Get data from Amazon S3 instead of loading from local disk
 print("Loading tiny_shakespeare dataset")
-current_dataset = load_dataset("tiny_shakespeare")
-ray_datasets = ray.data.from_huggingface(current_dataset['train'])
+ray_datasets = load_dataset("tiny_shakespeare")
+# ray_datasets = ray.data.from_huggingface(current_dataset)
 block_size = 512
 
 
